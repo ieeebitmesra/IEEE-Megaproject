4f29b356e8a55beef865ce9efb6b73d1a69191c0
Hardik Gupta <hardikgupta0506@gmail.com>
2022-01-09 22:30:37 +0530

    commit

--- Bitastik/pages/calender/index.js
+++ Bitastik/pages/calender/index.js
@@ -8,9 +8,12 @@ const  CalendarComponents=()=> {
         <div style={{
             width: '90vw',
             color: "black",
-            backgroundColor:"grey",
+            backgroundColor: "#b8c6db",
+            backgroundImage: "linear-gradient(315deg, #b8c6db 0%, #f5f7fa 74%)",
+            borderRadius:"15px",
             padding:"20px",
             overflow:"hidden",
+
             // backgroundImage:`url(${Books})`
         }}>
          
